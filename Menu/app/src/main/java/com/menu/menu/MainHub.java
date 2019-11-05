3017e105e3caee81df1be8b777be3a596e733c04
Elliot <elliot.one.harding@gmail.com>
2019-11-05 22:13:57 +0000

    Setup settings link

--- Menu/app/src/main/java/com/menu/menu/MainHub.java
+++ Menu/app/src/main/java/com/menu/menu/MainHub.java
@@ -41,7 +41,7 @@ public class MainHub extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_hub, menu);
+        //getMenuInflater().inflate(R.menu.main_hub, menu);
         return true;
     }
 
