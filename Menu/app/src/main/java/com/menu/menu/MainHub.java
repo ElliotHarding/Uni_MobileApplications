54f5ccd7d0ee90041f900b5ed4338f10d4702da5
Elliot <elliot.one.harding@gmail.com>
2019-11-06 14:58:37 +0000

    Save

--- Menu/app/src/main/java/com/menu/menu/MainHub.java
+++ Menu/app/src/main/java/com/menu/menu/MainHub.java
@@ -17,8 +17,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.menu.menu.Classes.DatabaseCommunicator;
+import com.menu.menu.Classes.LocalSettings;
 
 public class MainHub extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
@@ -35,6 +37,12 @@ public class MainHub extends AppCompatActivity implements NavigationView.OnNavig
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        TextView title = navigationView.getHeaderView(0).findViewById(R.id.nav_bar_title);
+        title.setText(LocalSettings.LocalUser.Username);
+
+        TextView subTitle = navigationView.getHeaderView(0).findViewById(R.id.nav_bar_subTitle);
+        subTitle.setText(LocalSettings.LocalUser.Email);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
