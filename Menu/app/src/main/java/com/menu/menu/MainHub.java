7b2214c6a046cf3cc0e4e2a5e1e7a65417b64241
Elliot <elliot.one.harding@gmail.com>
2019-11-06 14:36:34 +0000

    Lolz

--- Menu/app/src/main/java/com/menu/menu/MainHub.java
+++ Menu/app/src/main/java/com/menu/menu/MainHub.java
@@ -12,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -21,7 +22,6 @@ import com.menu.menu.Classes.DatabaseCommunicator;
 
 public class MainHub extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
-
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
@@ -34,14 +34,11 @@ public class MainHub extends AppCompatActivity implements NavigationView.OnNavig
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_chefSettigns, R.id.nav_settings, R.id.nav_signOut)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
