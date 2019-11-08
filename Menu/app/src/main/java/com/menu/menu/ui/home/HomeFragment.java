bc34dbe6a12524883bd26d3137e53baacb3e7ef0
Elliot <elliot.one.harding@gmail.com>
2019-11-08 23:05:00 +0000

    Working map

--- Menu/app/src/main/java/com/menu/menu/ui/home/HomeFragment.java
+++ Menu/app/src/main/java/com/menu/menu/ui/home/HomeFragment.java
@@ -4,24 +4,92 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.support.annotation.Nullable;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.menu.menu.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment
+{
+    private MapView m_MapView;
+    private GoogleMap m_googleMap;
+    private HomeViewModel m_homeViewModel;
 
-    private HomeViewModel homeViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        m_homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        m_MapView = root.findViewById(R.id.mapView);
+        m_MapView.onCreate(savedInstanceState);
+        m_MapView.onResume();
+
+        try
+        {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        m_MapView.getMapAsync(new OnMapReadyCallback()
+        {
+            @Override
+            public void onMapReady(GoogleMap mMap)
+            {
+                m_googleMap = mMap;
+
+                // For showing a move to my location button
+                //m_googleMap.setMyLocationEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34, 151);
+                m_googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                m_googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
         return root;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        m_MapView.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        m_MapView.onPause();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        m_MapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        m_MapView.onLowMemory();
     }
 }
