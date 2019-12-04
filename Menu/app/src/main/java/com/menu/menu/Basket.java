cf657a1f6bfd62d1f7605524c719a7ac888a0806
Elliot <elliot.one.harding@gmail.com>
2019-12-04 05:20:44 +0000

    Working progress

--- Menu/app/src/main/java/com/menu/menu/Basket.java
+++ Menu/app/src/main/java/com/menu/menu/Basket.java
@@ -1,5 +1,6 @@
 package com.menu.menu;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,11 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.menu.menu.Classes.BasketListViewItem;
 import com.menu.menu.Classes.DatabaseCommunicator;
+import com.menu.menu.Classes.Meal;
 import com.menu.menu.Classes.MealsCallback;
 import com.menu.menu.Classes.Order;
 
@@ -107,5 +111,87 @@ public class Basket extends Fragment
     private void SetError(String errorString)
     {
         Toast.makeText(getActivity(), errorString,  Toast.LENGTH_LONG).show();
+    }
+
+    //Class used to create a corresponding UI element for each Meal in an arraylist of meals
+    //These UI elements are then added into displayList
+    class BasketListViewItem extends ArrayAdapter<Order>
+    {
+        ArrayList<Order> m_orderListRef;
+
+        public BasketListViewItem(Context c, ArrayList<Order> orderListRef)
+        {
+            super(c, R.layout.layout_meal_info, orderListRef);
+            m_orderListRef = orderListRef;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            //get view from activity_forms.xml
+            View itemView = convertView;
+            if (itemView == null)
+            {
+                itemView = (LayoutInflater.from(getContext())).inflate(R.layout.layout_basket_item, parent, false);
+            }
+
+            final Order currentOrder = m_orderListRef.get(position);
+            final Meal currentMeal = currentOrder.GetMeal();
+
+            //Subject text
+            final TextView txt_subject = itemView.findViewById(R.id.li_top);
+            txt_subject.setText(currentMeal.getName());
+
+            //Ammount
+            final TextView txt_orderAmount = itemView.findViewById(R.id.li_amount);
+            txt_orderAmount.setText(currentOrder.GetNumberOfMeals());
+
+            //Price
+            ((TextView)itemView.findViewById(R.id.li_text)).setText(currentMeal.getPrice() + "£");
+
+            //Img
+            ImageView img = itemView.findViewById(R.id.img);
+            img.setImageBitmap(currentMeal.getPicture());
+
+            itemView.findViewById(R.id.btn_portionsAdd).setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    int mealNumber = currentOrder.GetNumberOfMeals_n();
+                    if(mealNumber < currentOrder.GetNumberOfMeals_n())
+                    {
+                        mealNumber+=1;
+                        currentOrder.SetNumberOfMeals_n(mealNumber);
+                        txt_orderAmount.setText(currentOrder.GetNumberOfMeals());
+                    }
+                }
+            });
+
+            itemView.findViewById(R.id.btn_portionsSub).setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    int mealNumber = currentOrder.GetNumberOfMeals_n();
+                    if(mealNumber > 0)
+                    {
+                        mealNumber-=1;
+                        currentOrder.SetNumberOfMeals_n(mealNumber);
+                    }
+                }
+            });
+
+            itemView.findViewById(R.id.btn_remove).setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    m_orderListRef.remove(this);
+                }
+            });
+
+            return itemView;
+        }
     }
 }
